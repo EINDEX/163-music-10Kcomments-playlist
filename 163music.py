4d8d82ff9f2a6bfeca1f9064b3f3840f201f8a23
EINDEX <snowstarlbk@gmail.com>
2016-09-04 00:34:31 +0800

    Update 163music.py

--- 163music.py
+++ 163music.py
@@ -1,6 +1,5 @@
 import binascii
 import multiprocessing
-import pymysql
 import requests
 import json
 import os
@@ -13,28 +12,30 @@ from queue import Queue
 from threading import Thread
 
 from pymongo import MongoClient
-from requests import HTTPError
 
 modulus = '00e0b509f6259df8642dbc35662901477df22677ec152b5ff68ace615bb7b725152b3ab17a876aea8a5aa76d2e417629ec4ee341f56135fccf695280104e0312ecbda92557c93870114af6c9d05c4f7f0c3685b7a46bee255932575cce10b424d813cfe4875d3e82047b97ddef52741d546b8e289dc6935b3ece0462db0a22b8e7'
 nonce = '0CoJUm6Qyw8W8jud'
 pubKey = '010001'
 
+playlist_url = 'http://music.163.com/api/playlist/detail?id=%s'
 song_url = 'http://music.163.com/api/song/detail/?ids=%s'
-comment_url = 'http://music.163.com/weapi/v1/resource/comments/'
+comment_url = 'http://music.163.com/weapi/v1/resource/comments/%s'
 
 headers = {
     'Cookie': 'appver=1.5.0.75771;',
     'Referer': 'http://music.163.com/'
 }
+
+
+
 text = {
     'username': os.environ['USERNAME'],
     'password': os.environ['PASSWORD'],
     'rememberLogin': 'true'
 }
 
-client = MongoClient()
+client = MongoClient("mongodb://u9SYvucFl075xtZr:pZrxzsykLUDeud6Qh@10.10.190.60:27017")
 db = client['163music']
-song_id = 60000
 
 
 def aesEncrypt(text, secKey):
@@ -68,56 +69,38 @@ def login():
     }
 
 
-def get_music_comments(ids):
-    """获取评论"""
-    time.sleep(500)
-    url = comment_url + str(ids)
-    req_json = requests.post(url, headers=headers, data=data).json()
-    return req_json
-
-
-def get_song_by_playid(queue):
-    global song_id, times
-    req_json = requests.get(song_url % str([ids for ids in range(song_id, song_id + 50)])).json()
-    if req_json['code'] == 200:
-        for song in req_json['songs']:
-            if song['commentThreadId'] is not None:
-                queue.put(
-                    {
-                        'id': song['id'],
-                        'name': song['name'],
-                        'comment_url': song['commentThreadId'],
-                        'mp3Url': song['mp3Url'],
-                        'mv': song['mvid']
-                    })
-    song_id += 50
-
-
 def create_music(q):
     while True:
-        get_song_by_playid(q)
-
-
-def insert(insert_data):
-    print(insert_data)
-    songs = db.songs
-    songs.update({'_id': insert_data['id']}, insert_data, True)
+        playlist = db.playlist.find_one({'_id': 'playlist'})['id']
+        req_json = requests.get(playlist_url % str([ids for ids in range(playlist, playlist + 50)])).json()
+        if req_json['code'] == 200:
+            for song in req_json['songs']:
+                if song['commentThreadId'] is not None:
+                    queue.put(song)
+        db.playlist.update({'_id': 'playlist'}, {'id': playlist + 1})
 
 
 def get_comment(q, ):
     while True:
+        time.sleep(1000)
         music_dict = q.get()
-        ids_comments = get_music_comments(music_dict['comment_url'])
-        if ids_comments is None:
+        ids_comments = requests.post(comment_url % str(music_dict['commentThreadId']),
+                                     headers=headers,
+                                     data=data).json()
+        if ids_comments['code'] != 200:
             return
         music_dict['comments'] = ids_comments['hotComments']
         music_dict['comment_total'] = ids_comments['total']
-        insert(music_dict)
+        music_dict['_id'] = music_dict['id']
+        songs = db.songs
+        songs.save(music_dict)
 
 
 if __name__ == '__main__':
     print('模块', sys.argv[0])
     queue = Queue(maxsize=50)
+    if db.playlist.find_one({'_id': 'playlist'}) is None:
+        db.playlist.insert({'_id': 'playlist', 'playlist': 100001})
     login()
     c = Thread(name='歌曲信息', target=create_music, args=(queue,))
     c.start()
